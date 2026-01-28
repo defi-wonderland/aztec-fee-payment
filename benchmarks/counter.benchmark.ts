7558b1d24023103d93d7e229c8a82b469733e507
Weißer Hase <wei3erHase@protonmail.com>
2026-01-28 15:09:26 -0300

    fix: adding process exit at benchmark teardown

--- benchmarks/counter.benchmark.ts
+++ benchmarks/counter.benchmark.ts
@@ -230,4 +230,8 @@ export default class CounterContractBenchmark extends Benchmark {
 
     return methods;
   }
+
+  async teardown(context: BenchmarkContext): Promise<void> {
+    process.exit(0);
+  }
 }
