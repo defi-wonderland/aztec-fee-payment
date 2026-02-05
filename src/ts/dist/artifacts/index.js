2eb6e721bcd72994c8d6a21bf7d9b50767f74e1e
xorsal <zorzal@wonderland.xyz>
2026-02-05 22:10:08 +0000

    chore: remove src/ts/dist/

--- src/ts/dist/artifacts/index.js
+++
@@ -1,5 +0,0 @@
-// Re-export all contract artifacts and types
-export { MeteredContract, MeteredContractArtifact } from "./Metered.js";
-// Test utilities (not part of main API)
-export { CounterContract, CounterContractArtifact } from "./Counter.js";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9hcnRpZmFjdHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsNkNBQTZDO0FBQzdDLE9BQU8sRUFBRSxlQUFlLEVBQUUsdUJBQXVCLEVBQUUsTUFBTSxjQUFjLENBQUM7QUFFeEUsd0NBQXdDO0FBQ3hDLE9BQU8sRUFBRSxlQUFlLEVBQUUsdUJBQXVCLEVBQUUsTUFBTSxjQUFjLENBQUMifQ==
