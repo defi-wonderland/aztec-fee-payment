def90aa7580eaa394c254f723fd65bcdb0e6c05d
0xLeopoldo <leopoldo@wonderland.xyz>
2026-07-01 16:23:16 -0300

    chore: upgrade 5.0.0-rc.1 (#155)

--- src/ts/test/harness.ts
+++ src/ts/test/harness.ts
@@ -79,7 +79,7 @@ export type FundFeeJuiceFromL1Options = {
  * This is the standard way to fund FPCs with the native fee token.
  */
 export async function fundL2AddressWithFeeJuiceFromL1(
-  aztecNode: Pick<AztecNode, "getL1ToL2MessageCheckpoint" | "getBlock">,
+  aztecNode: Pick<AztecNode, "getL1ToL2MessageCheckpoint" | "getBlockData">,
   wallet: Wallet,
   recipient: AztecAddress,
   opts: FundFeeJuiceFromL1Options,
@@ -179,7 +179,7 @@ export type BridgeForMintResult = {
 export async function bridgeForMint(
   aztecNode: Pick<
     AztecNode,
-    "getL1ToL2MessageCheckpoint" | "getBlock" | "getNodeInfo"
+    "getL1ToL2MessageCheckpoint" | "getBlockData" | "getNodeInfo"
   >,
   fpcAddress: AztecAddress,
   claimer: AztecAddress,
